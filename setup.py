6d07b30658aef2b683db33a357f05dc261dd63a7
piotrkica <piotr.kica.35@gmail.com>
2022-05-15 22:21:35 +0200

    python module setup files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='cloudvvuq',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Run simulations on cloud with EasyVVUQ functionality ',
+    install_requires=['easyvvuq', 'backoff', 'aiohttp', 'tqdm']
+)
