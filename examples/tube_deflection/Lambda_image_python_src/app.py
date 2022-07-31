018dd84b17636b1610520cd9783888311a58020a
piotrkica <piotr.kica.35@gmail.com>
2022-07-31 19:49:11 +0200

    add aws lambda image source code for TubeDeflection

---
+++ examples/tube_deflection/Lambda_image_python_src/app.py
@@ -0,0 +1,31 @@
+import os
+import json
+import subprocess
+
+
+def handler(event, context):
+    input_json = json.loads(event['body'])
+    input_file = f"/tmp/input_{input_json['input_id']}.json"
+    input_json["outfile"] = f"/tmp/output_{input_json['input_id']}.json"
+
+    with open(input_file, "w+") as f:
+        json.dump(input_json, f)
+
+    subprocess.run(["python3", "beam.py", input_file], capture_output=True)
+
+    with open(input_json["outfile"]) as f:
+        result = json.load(f)
+
+    result.update(input_json)
+
+    os.remove(input_file)
+    os.remove(input_json["outfile"])
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(result),
+        "headers": {
+            "content-type": "application/json"
+        },
+        "isBase64Encoded": False
+    }
